55e3dc881b91e97c929decdaa3d97372bb7aa742
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-21 22:35:23 +0530

    input selectors

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 var btn = document.querySelector("#btn-submit")
 var inputText = document.querySelector("#input-box")
+var nameText = document.querySelector("#input-name");
 
 console.log(inputText)
 function clickHandler() {
     console.log("Click..");
     console.log("input", inputText.value);
+    console.log("Name:", nameText.value)
 };
 btn.addEventListener("click", clickHandler)
