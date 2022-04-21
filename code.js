28adfa3ae99c44e2f4f12a8f0b6c5b75613e8f92
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-21 22:39:36 +0530

    pushing

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 var btn = document.querySelector("#btn-submit")
 var inputText = document.querySelector("#input-box")
 var nameText = document.querySelector("#input-name");
-
+var output = document.querySelector("#output")
+// attribute selector: "input[name='translator']" 
 console.log(inputText)
+console.log(output)
 function clickHandler() {
     console.log("Click..");
     console.log("input", inputText.value);
     console.log("Name:", nameText.value)
+
 };
 btn.addEventListener("click", clickHandler)
