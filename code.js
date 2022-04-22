51af557985ba2b313bdf1ab9262a531719a53b02
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-22 20:22:18 +0530

    api_stuff

--- code.js
+++ code.js
@@ -1,13 +1,26 @@
-var btn = document.querySelector("#btn-submit")
-var inputText = document.querySelector("#input-box")
-// var nameText = document.querySelector("#input-name");
-var outputText = document.querySelector("#output")
-// attribute selector: "input[name='translator']" 
+var btn = document.querySelector("#btn-submit") //button
+var inputText = document.querySelector("#input-box") //input
+var outputText = document.querySelector("#output") //output
 
+// attribute selector: "input[name='translator']" 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getResponseText(input) {
+    return serverURL + "?" + "text=" + input
+};
 function clickHandler() {
 
-    outputText.innerText = "Translated " + inputText.value;
-
-
+    var input = inputText.value; //input
+    fetch(getResponseText(input)) //processing
+        .then(response => response.json())
+        .then(json => {
+            var transText = json.contents.translated;
+            outputText.innerText = transText; //output
+        })
+        .catch(errorHandler) //Calling server
+};
+function errorHandler(error) {
+    console.log("Error Occured ... " + error)
+    alert("Something is wrong! Try again later")
 };
 btn.addEventListener("click", clickHandler)
