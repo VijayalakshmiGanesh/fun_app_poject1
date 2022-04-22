6b4bcbf7d490c4a1bdda2ea6d0fc69aff4a3a984
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-22 23:13:07 +0530

    output without text format

--- APIproj.js
+++ APIproj.js
@@ -7,6 +7,27 @@ button.addEventListener("click", showLyrics)
 var musicURL = "https://api.lyrics.ovh/v1/"
 
 function showLyrics() {
-    var artistName = artist.value;
-    console.log(artistName)
+    var input = artist.value + "/" + song.value;
+
+    // //processing
+    // fetch(getResponseText(input))
+    // .then(response => response.json())
+    // .then(json => {
+    //     var lyrics = json.contents
+    // })
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://api.lyrics.ovh/v1/' + input);
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            output.innerText = this.responseText;
+        }
+    };
+
+    request.send();
+
+
+
 };
