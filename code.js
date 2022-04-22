532d3d3527e4ac4b02c9f8364b0a1745136fba3d
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-22 12:15:09 +0530

    input output process

--- code.js
+++ code.js
@@ -1,14 +1,13 @@
 var btn = document.querySelector("#btn-submit")
 var inputText = document.querySelector("#input-box")
-var nameText = document.querySelector("#input-name");
-var output = document.querySelector("#output")
+// var nameText = document.querySelector("#input-name");
+var outputText = document.querySelector("#output")
 // attribute selector: "input[name='translator']" 
-console.log(inputText)
-console.log(output)
+
 function clickHandler() {
-    console.log("Click..");
-    console.log("input", inputText.value);
-    console.log("Name:", nameText.value)
+
+    outputText.innerText = "Translated " + inputText.value;
+
 
 };
 btn.addEventListener("click", clickHandler)
