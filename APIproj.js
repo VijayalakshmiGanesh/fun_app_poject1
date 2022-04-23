cb889c46da97657b50ad5fdae5b2653faf2e4ba7
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-04-23 13:01:49 +0530

    testing js in live

--- APIproj.js
+++ APIproj.js
@@ -4,7 +4,7 @@ var food = document.querySelector("#food")
 var recipe = document.querySelector("#recipe")
 
 food.addEventListener("click", showFood)
-
+console.log("Inside js file")
 function showFood() {
 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=` + foodName.value)
@@ -17,28 +17,6 @@ function showFood() {
                 recipe.innerHTML = "<b>Instructions to cook: </b>" + ins;
             })
         })
-
-
-};
-
-
-function showLyrics() {
-    var input = artist.value + "/" + song.value;
-
-
-
-    var request = new XMLHttpRequest();
-
-    request.open('GET', 'https://api.lyrics.ovh/v1/' + input);
-
-    request.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            output.innerText = this.responseText;
-        }
-    };
-
-    request.send();
-
 
 
 };
